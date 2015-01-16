8772f8cf916de5743baaba1f4f2350a7a893ecba
bucho <steelwind666@gmail.com>
2015-01-17 07:54:31 +0900

    4人同時プレイまで実装

--- walkdemo.py
+++ walkdemo.py
@@ -6,7 +6,6 @@ from pygameframework import Color
 from pygameframework import AsciiTileSheet
 from pygameframework import Coordinate
 from pygameframework import Direction
-from pygameframework import Sprite
 import sys
 
 class ActorMap(object):
@@ -25,6 +24,12 @@ class ActorMap(object):
             return self._actor[coordinate]
         return None
 
+    def coordinate_of(self, actor):
+        try:
+            return self._coordinate[actor]
+        except KeyError:
+            return None
+
     def put(self, coordinate, actor):
         self._actor[coordinate] = actor
         self._coordinate[actor] = coordinate
@@ -33,7 +38,7 @@ class ActorMap(object):
         for pos, actor in self._actor.items():
             actor.render(pos, screen)
 
-    def moved_position(self, actor, direction):
+    def to_coordinate(self, actor, direction):
         c = self._coordinate[actor]
         return c + direction
 
@@ -48,36 +53,63 @@ class Actor(object):
     def render(self, position, screen):
         screen.draw(position, self._graphic)
 
-class WalkDemo(Game):
-    POSITION = Coordinate(0, 0)
-    GRID_SIZE = Coordinate(10, 18)
+class MapHandler(object):
+    active_map = ActorMap(80, 20)
+
     def __init__(self):
-        Game.__init__(self)
-        self._window = None
-        self._actor = None
-        self._map = ActorMap(80, 20)
+        self._map = self.active_map
+
+class PlayerHandler(MapHandler):
+    def __init__(self, actor):
+        MapHandler.__init__(self)
+        self._actor = actor
 
     def initialize(self):
-        tile_sheet = AsciiTileSheet().initialize('Courier New', 18)
-        self._actor = Actor(tile_sheet.get_tile('@', Color.LIME))
-        self._map.put(Coordinate(1, 1), self._actor)
-        self._map.put(Coordinate(1, 2),
-                Actor(tile_sheet.get_tile('@', Color.AQUA)))
-        self._map.put(Coordinate(2, 2),
-                Actor(tile_sheet.get_tile('@', Color.OLIVE)))
-    
-    def update(self):
-        down_keys =  self._controllers[0].pressed_keys()
+        x = 1
+        while self._map.actor(Coordinate(x, 1)): x += 1
+        self._map.put(Coordinate(x, 1), self._actor)
+        return self
+
+    def handle(self, controller, keyboard=None):
+        down_keys =  controller.pressed_keys()
         if 'left' in down_keys: self._walk(Direction.LEFT)
         if 'down' in down_keys: self._walk(Direction.DOWN)
         if 'up' in down_keys: self._walk(Direction.UP)
         if 'right' in down_keys: self._walk(Direction.RIGHT)
-
-        down_keys =  self._keyboard.pressed_keys()
+        if 'start' in down_keys: self.initialize()
+        if not keyboard: return
+        down_keys =  keyboard.pressed_keys()
         if ord('h') in down_keys: self._walk(Direction.LEFT)
         if ord('j') in down_keys: self._walk(Direction.DOWN)
         if ord('k') in down_keys: self._walk(Direction.UP)
         if ord('l') in down_keys: self._walk(Direction.RIGHT)
+        if ord('q') in down_keys: sys.exit()
+
+    def _walk(self, direction):
+        pos = self._map.to_coordinate(self._actor, direction)
+        if self._map.actor(pos): return
+        self._map.move_actor(self._actor, direction)
+
+class WalkDemo(Game, MapHandler):
+    POSITION = Coordinate(0, 0)
+    GRID_SIZE = Coordinate(10, 18)
+    MAX_PLAYER = 4
+    PLAYER_COLOR = (Color.RED, Color.AQUA, Color.LIME, Color.YELLOW)
+    def __init__(self):
+        Game.__init__(self)
+        MapHandler.__init__(self)
+        self._window = None
+        self._handlers = []
+
+    def initialize(self):
+        tile_sheet = AsciiTileSheet().initialize('Courier New', 18)
+        for color in self.PLAYER_COLOR:
+            actor = Actor(tile_sheet.get_tile('@', color))
+            self._handlers.append(PlayerHandler(actor).initialize())
+
+    def update(self):
+        for handler, controller in zip(self._handlers, self._controllers):
+            handler.handle(controller, self._keyboard)
 
     def set_screen(self, screen):
         self._screen = screen
@@ -86,11 +118,6 @@ class WalkDemo(Game):
     def render(self):
         self._screen.fill()
         self._map.render(self._window)
-
-    def _walk(self, direction):
-        pos = self._map.moved_position(self._actor, direction)
-        if self._map.actor(pos): return
-        self._map.move_actor(self._actor, direction)
 
 if __name__ == '__main__':
     from pygameframework.framework import GameRunner
